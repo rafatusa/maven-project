ef66105d3d6ce60c4d0ead32339aa24ee8425830
rafatusa <piealrafat@gmail.com>
2017-12-25 21:49:06 -0500

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet(final String someone) {
+/**
+ * @param this is popo
+ * @return this is lolo
+ */
+  
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
